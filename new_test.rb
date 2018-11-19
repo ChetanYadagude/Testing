98da7a4fec3dfb406635c9a0d2046f473e3bf51b
Sanjiv <sanjivkumarverma12@gmail.com>
2018-11-19 14:25:07 +0530

    Update new_test.rb

--- new_test.rb
+++ new_test.rb
@@ -1,2 +1,3 @@
 api_git_commit1 = command "echo '#{git ls-remote https://github.com/ChetanYadagude/Sanjiv_project.git HEAD}' >> #{api_git_commit1}"
 puts "#{api_git_commit1}"
+puts "It is for testing";
