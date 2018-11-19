d5ec40c39eabe61c16fa50e25dfcf8e59b2d7631
Sanjiv <sanjivkumarverma12@gmail.com>
2018-11-19 14:57:51 +0530

    Update new_test.rb

--- new_test.rb
+++ new_test.rb
@@ -1,3 +1,3 @@
 puts "It is for testing";
 puts "testing again";
-puts system('git ls-remote https://github.com/ChetanYadagude/Testing/blob/master/new_test.rb HEAD')
+#puts system('git ls-remote https://github.com/ChetanYadagude/Testing/blob/master/new_test.rb HEAD')
